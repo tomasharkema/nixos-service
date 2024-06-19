977b7dbe5f88ba652d3cfa9b008d083672cde995
Tomas Harkema <tomas@harkema.io>
2024-06-19 12:36:06 +0200

    add main program

--- package.nix
+++ package.nix
@@ -1,9 +1,11 @@
 {buildGoModule}:
-buildGoModule {
+buildGoModule rec {
   pname = "nixos-service";
   version = "0.0.1";
 
   src = ./src;
 
   vendorHash = "sha256-ToAVii2yp+69PUwdpJr98S554yizQr+o0jB6XRKpI84=";
+
+  meta.mainProgram = "${pname}";
 }
